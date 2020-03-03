5a7a31e5aee8a32f805fb6ba249710391d421bee
djjtyn <djjtynan@gmail.com>
2020-03-03 11:39:30 +0000

    authorisation functionality added

--- myWebsite/settings.py
+++ myWebsite/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'myWebsite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,3 +120,21 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
+MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = os.environ.get("EMAIL_ADDRESS")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASSWORD")
+EMAIL_PORT = 587
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'accounts.backends.EmailAuth'
+]
--- readme.md
+++ readme.md
@@ -1,1 +1,3 @@
+# MS4
+
 [![Build Status](https://travis-ci.org/djjtyn/ms4.svg?branch=master)](https://travis-ci.org/djjtyn/ms4)
