17095a2168e1933612b005c32f9284232dbe49e1
djjtyn <djjtynan@gmail.com>
2020-03-12 18:39:30 +0000

    google maps now working in base footer

---
+++ static/js/scripts.js
@@ -0,0 +1,15 @@
+function initMap() {
+    var location = {lat: 53.458479,lng: -6.219895};
+    var map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 15,
+        center: location
+    });
+    var marker = new google.maps.Marker({
+    position: location,
+    map: map,
+    title: 'Hello World!'
+  });
+}
+
+
+  
