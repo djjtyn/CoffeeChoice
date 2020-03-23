88b1b26d8a248b4f2f469bd692db48811be2b48c
djjtyn <djjtynan@gmail.com>
2020-03-23 10:51:12 +0000

    index.html rendering in home app

--- myWebsite/urls.py
+++ myWebsite/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import url, include
 from django.contrib import admin
-from accounts.views import index
+from home import urls as home_urls
 from accounts import urls as accounts_urls
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', index, name="index"),
+    url(r'^', include(home_urls)),
     url(r'^accounts/', include(accounts_urls))
 ]
