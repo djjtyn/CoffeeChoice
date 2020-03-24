6ae8c5fa9e3c1f07132963430183689f0b8a09d6
djjtyn <djjtynan@gmail.com>
2020-03-24 15:43:56 +0000

    intensity model added to coffee app

--- coffee/admin.py
+++ coffee/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Range, Coffee
+from .models import Range, Coffee, Intensity
 
 # Register your models here.
 admin.site.register(Range)
 admin.site.register(Coffee)
+admin.site.register(Intensity)
--- coffee/models.py
+++ coffee/models.py
@@ -8,10 +8,16 @@ class Range(models.Model):
     def __str__(self):
         return self.name
 
+class Intensity(models.Model):
+    intensity = models.IntegerField(blank=True, null=True)
+
+    def __str__(self):
+        return str(self.intensity)
+
 
 class Coffee(models.Model):
     name = models.CharField(max_length=40, blank=False)
-    intensity = models.PositiveIntegerField()
+    intensity = models.ForeignKey(Intensity,default=1, on_delete=models.CASCADE)
     description = models.TextField()
     image = models.ImageField(upload_to='images')
-    coffee_range = models.ForeignKey(Range, default=1)
+    coffee_range = models.ForeignKey(Range, default=1, on_delete=models.CASCADE)
