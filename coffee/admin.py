f00b7cedb6fe4f1a2842c0fddff853e703aa1e58
djjtyn <djjtynan@gmail.com>
2020-03-24 12:37:36 +0000

    range model created and linked to coffee model

--- coffee/admin.py
+++ coffee/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Range, Coffee
 
 # Register your models here.
+admin.site.register(Range)
+admin.site.register(Coffee)
--- coffee/models.py
+++ coffee/models.py
@@ -1,10 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Range(models.Model):
+    name = models.CharField(max_length=35, blank=False)
+
+    def __str__(self):
+        return self.name
+
+
 class Coffee(models.Model):
-    name = models.CharField(max_length=30, blank=False)
-    original_range = models.BooleanField(default=False)
-    vertuo_range = models.BooleanField(default=False)
-    intensity = models.IntegerField(max_value=10)
+    name = models.CharField(max_length=40, blank=False)
+    intensity = models.PositiveIntegerField()
     description = models.TextField()
     image = models.ImageField(upload_to='images')
+    coffee_range = models.ForeignKey(Range, default=1)
