e1d00742323623c4108ee965c1989d99d7136c23
djjtyn <djjtynan@gmail.com>
2020-05-05 13:54:53 +0000

    pagination buttons fixed for smaller screens

--- myWebsite/settings.py
+++ myWebsite/settings.py
@@ -29,7 +29,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['coffechoice.herokuapp.com']
+ALLOWED_HOSTS = ['coffechoice.herokuapp.com',
+                 'localhost']
 
 
 # Application definition
