815cf7e6abbc6e315cb0880cabf73e14b623adf4
djjtyn <djjtynan@gmail.com>
2020-05-12 15:32:12 +0000

    comments added to accounts app

--- accounts/test_views.py
+++ accounts/test_views.py
@@ -1,11 +1,13 @@
 from django.test import TestCase
 
 class TestViews(TestCase):
+    """test to check login page is using correct template"""
     def test_get_login_page(self):
         page = self.client.get('/accounts/login/')
         self.assertEqual(page.status_code, 200)
         self.assertTemplateUsed(page, 'login.html')
 
+    """test to check registration page is using correct template"""
     def test_get_registration_page(self):
         page = self.client.get('/accounts/register/')
         self.assertEqual(page.status_code, 200)
