199efea13147f903139f808a6048fe360f3b322d
djjtyn <djjtynan@gmail.com>
2020-05-12 16:35:04 +0000

    comments added to cart app

--- cart/test_views.py
+++ cart/test_views.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 
 class TestViews(TestCase):
+    """test to check cart page returns correct template"""
     def test_get_cart_page(self):
         page = self.client.get('/cart/')
         self.assertEqual(page.status_code, 200)
